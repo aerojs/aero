be382a89e906a1b4918a989e475f1e880886106e
Eduard Urbach <e.urbach@gmail.com>
2015-05-19 12:21:13 +0900

    Template only pages, fixes #1

--- src/classes/Page.js
+++ src/classes/Page.js
@@ -7,18 +7,34 @@ let Page = function(id, pagePath) {
 	this.modulePath = path.resolve(path.join(this.path, id + ".js"));
 	this.templatePath = path.resolve(path.join(this.path, id + ".jade"));
 	
-	// Delete existing controller from cache
-	delete require.cache[this.modulePath];
-	
-	// Reload controller
-	this.controller = require(this.modulePath);
-	
 	// Load template
 	try {
 		this.renderTemplate = jade.compileFile(this.templatePath);
 	} catch(e) {
 		if(e.code !== "ENOENT")
 			console.error(e);
+	}
+	
+	// Delete existing controller from cache
+	delete require.cache[this.modulePath];
+	
+	// Load controller
+	try {
+		this.controller = require(this.modulePath);
+	} catch(e) {
+		if(e.code !== "MODULE_NOT_FOUND")
+			console.error(e);
+		
+		// Default static page controller
+		if(this.renderTemplate) {
+			this.controller = {
+				code: this.renderTemplate(),
+				
+				get: function(request, response) {
+					response.end(this.code);
+				}
+			};
+		}
 	}
 };
 
