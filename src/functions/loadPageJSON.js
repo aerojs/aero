b565ae0489e41c3eec431f17daa3d2a671a1d847
Eduard Urbach <e.urbach@gmail.com>
2015-06-03 23:47:57 +0900

    Added more test cases

--- src/functions/loadPageJSON.js
+++ src/functions/loadPageJSON.js
@@ -9,14 +9,7 @@ Promise.promisifyAll(fs);
 // loadPageJSON
 let loadPageJSON = function(jsonPath) {
 	return fs.readFileAsync(jsonPath, 'utf8').then(function(fileContents) {
-		try {
-			return Promise.resolve(JSON.parse(fileContents));
-		} catch(e) {
-			if(e)
-				console.error(e);
-
-			return Promise.resolve(null);
-		}
+		return Promise.resolve(JSON.parse(fileContents));
 	}).error(function(readError) {
 		if(readError.code !== 'ENOENT')
 			console.error(readError);
--- test/functions.js
+++ test/functions.js
@@ -1,11 +1,28 @@
 'use strict';
 
-let loadPageJSON = require('../src/functions/loadPageJSON');
+let assert = require('assert');
 
 describe('functions', function() {
 	describe('loadPageJSON', function() {
-		it('should not throw when the JSON file is defect', function() {
-			loadPageJSON('test/defect.json');
+		let loadPageJSON = require('../src/functions/loadPageJSON');
+		
+		it('should not throw when the JSON file does not exist', function(done) {
+			loadPageJSON('test/does not exist.json').then(done);
+		});
+		
+		it('should not throw when the JSON file is defect', function(done) {
+			loadPageJSON('test/defect.json').then(done);
+		});
+	});
+	
+	describe('loadFavIcon', function() {
+		let loadFavIcon = require('../src/functions/loadFavIcon');
+		
+		it('should not throw when the JSON file does not exist', function(done) {
+			loadFavIcon('test/does not exist.ico', function(imageData) {
+				assert(imageData === null);
+				done();
+			});
 		});
 	});
 });
