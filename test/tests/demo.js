39e27bc5ece73a9492280b39325fa8c60a03456f
Eduard Urbach <e.urbach@gmail.com>
2016-02-07 16:27:07 +0900

    Improved starter config

--- test/tests/demo.js
+++ test/tests/demo.js
@@ -1,5 +1,6 @@
 test('App: Demo', Promise.coroutine(function*(t) {
 	let app = aero('test/apps/demo')
+	app.verbose = false
 
 	yield app.run()
 	t.appOk(app)
--- test/tests/empty.js
+++ test/tests/empty.js
@@ -1,13 +1,12 @@
 test('App: Empty', Promise.coroutine(function*(t) {
 	rmdir('test/apps/empty', false, ['.gitignore'])
 
-	let app = aero('test/apps/demo')
+	let app = aero('test/apps/empty')
+	app.verbose = false
 
 	yield app.run()
 	yield app.restart()
 	t.appOk(app)
 	t.pass('app.restart')
 	yield app.stop()
-
-	rmdir('test/apps/empty', false, ['.gitignore'])
 }))
